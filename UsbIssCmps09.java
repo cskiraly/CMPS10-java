c6008e5d71e12004fbc462b00793b926a9953ec9
Csaba Kiraly <kiraly@disi.unitn.it>
2012-03-22 19:03:23 +0100

    fixing problems due to the non-blocking read in TOSSerial

--- UsbIssCmps09.java
+++ UsbIssCmps09.java
@@ -1,27 +1,43 @@
 import java.io.*;
 import net.tinyos.comm.*;
 
-public class UsbIssCmps09 {
+public class UsbIssCmps09 implements SerialPortListener {
 
     private SerialPort port;
     private String portName;
     private InputStream in;
     private OutputStream out;
 
-    public UsbIssCmps09(String portName) throws IOException, UnsupportedCommOperationException{
-    	this.portName = portName;
-    	open();
-    	setI2CMode();
+    Object readBlock = new Object();
+
+    public UsbIssCmps09(String portName) throws IOException, UnsupportedCommOperationException {
+       this.portName = portName;
+       open();
+       setI2CMode();
     }
 
     private void open() throws IOException, UnsupportedCommOperationException {
         System.out.println("Opening port " + portName);
         port = new TOSSerial(portName);
+
+
+
+
+        try {
+            //port.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+            port.setSerialPortParams(19200, 8, SerialPort.STOPBITS_2, false);
+            port.addListener(this);
+            port.notifyOn(SerialPortEvent.DATA_AVAILABLE, true);
+            port.notifyOn(SerialPortEvent.OUTPUT_EMPTY, true);
+        } catch (Exception e) {
+            port.close();
+            throw new IOException("Could not configure " + portName + ": "+ e.getMessage());
+        }
+
         in = port.getInputStream();
         out = port.getOutputStream();
- 
-        //port.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
-        port.setSerialPortParams(19200, 8, SerialPort.STOPBITS_2, false);
+
+        port.open();
     }
 
     private void setI2CMode() throws IOException {
@@ -31,22 +47,49 @@ public class UsbIssCmps09 {
     	        0x40,	// Set mode to 100KHz I2C
     	        0x00    // Spare pins set to output low
     	};
-    	int[] resp = new int[2];
+    	byte[] resp = new byte[2];
     	
         System.out.println("Writing " + cmd[0] + " " + cmd[1] + " " + cmd[2] + " " + cmd[3]);
     	out.write(cmd);
     	out.flush();
-    	for(int r = 0; r < resp.length; r++) {
-    		resp[r] = in.read();
-    		if (resp[r] < 0) {
-//        		System.out.println("**set_i2c_mode: read error");
-			r--;
-    		}
-    	}
+    	read(resp);
         System.out.println("Response " + resp[0] + " " + resp[1]);
-    	if (resp[0] != 0xFF) {
+    	if (resp[0] != (byte)0xFF) {
     		throw new IOException("**set_i2c_mode: Error setting I2C mode!**");
     	}
+  }
+
+  // TOSSerial's InputStream is not a real InputStream, it does not respect the semantics in that it is non-blocking.
+  // This is a helper function to implement the blocking variant, already for n bytes
+  private int read(byte[] b) throws IOException  {
+    int r = 0;
+
+    while (r < b.length) {
+      synchronized (readBlock) {
+        if (in.available() == 0) {
+          try {
+            readBlock.wait();
+          } catch (InterruptedException e) {	//continue the cycle
+          }
+        } else {
+          int ret = in.read(b);
+          if (ret == -1) {
+            return -1;
+          } else {
+            r += ret;
+          }
+        }
+      }
+    }
+    return r;
+  }
+
+  public void serialEvent(SerialPortEvent ev) {
+    if (ev.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+      synchronized (readBlock) {
+        readBlock.notify();
+      }
+    }
   }
 
     public int getDirection() throws IOException {
@@ -57,22 +100,16 @@ public class UsbIssCmps09 {
     			0x00,	// Register we want to read from (0 is software version)
     			0x06
     	};
-    	int[] resp = new int[6];
+    	byte[] resp = new byte[6];
 
     	out.write(cmd);
     	out.flush();
-    	for(int r = 0; r < resp.length; r++) {
-    		resp[r] = in.read();
-    		if (resp[r] < 0) {
-//        		System.out.println("**set_i2c_mode: read error");
-			r--;
-    		}
-    	}
-    	
+    	read(resp);
+
     	bearing = ((resp[2] << 8) + resp[3]) /10;
     	return bearing;
     }
-    
+
 	/**
 	 * @param args
 	 */
