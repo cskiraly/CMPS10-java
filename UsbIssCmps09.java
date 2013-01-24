f4c9a7472f43a5a828f4343dd4f01af9f37e7e30
Csaba Kiraly <kiraly@disi.unitn.it>
2013-01-24 22:32:44 +0100

    add copyright info

--- UsbIssCmps09.java
+++ UsbIssCmps09.java
@@ -1,3 +1,23 @@
+/*
+/* Copyright (c) 2012-2013 Csaba Kiraly http://csabakiraly.com
+ *
+ * This is a java driver for the CMPS09 tilt compensated compass.
+ *
+ * UsbIssCmps09 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * UsbIssCmps09 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with UsbIssCmps09.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import java.io.*;
 import net.tinyos.comm.*;
 
